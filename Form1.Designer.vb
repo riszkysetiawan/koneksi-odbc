45f9926b6044ad268b8655303aadfdd20e7bbf83
Riszkys <rizkysetiawann22@gmail.com>
2024-10-07 18:41:31 +0700

    first commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,217 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btmkoneksi = New Button()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        DataGridView1 = New DataGridView()
+        Column1 = New DataGridViewTextBoxColumn()
+        Column2 = New DataGridViewTextBoxColumn()
+        Column3 = New DataGridViewTextBoxColumn()
+        Column4 = New DataGridViewTextBoxColumn()
+        txtnim = New TextBox()
+        txtnama = New TextBox()
+        txtalamat = New TextBox()
+        txttelp = New TextBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' btmkoneksi
+        ' 
+        btmkoneksi.Location = New Point(637, 51)
+        btmkoneksi.Name = "btmkoneksi"
+        btmkoneksi.Size = New Size(140, 23)
+        btmkoneksi.TabIndex = 0
+        btmkoneksi.Text = "Cek Koneksi"
+        btmkoneksi.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(45, 29)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(83, 15)
+        Label1.TabIndex = 1
+        Label1.Text = "Kode Anggota"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(45, 59)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(39, 15)
+        Label2.TabIndex = 1
+        Label2.Text = "Nama"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(45, 93)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(45, 15)
+        Label3.TabIndex = 1
+        Label3.Text = "Alamat"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(45, 126)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(48, 15)
+        Label4.TabIndex = 1
+        Label4.Text = "Telepon"
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Columns.AddRange(New DataGridViewColumn() {Column1, Column2, Column3, Column4})
+        DataGridView1.Location = New Point(122, 225)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowTemplate.Height = 25
+        DataGridView1.Size = New Size(445, 213)
+        DataGridView1.TabIndex = 2
+        ' 
+        ' Column1
+        ' 
+        Column1.HeaderText = "Id Anggota"
+        Column1.Name = "Column1"
+        ' 
+        ' Column2
+        ' 
+        Column2.HeaderText = "Nama Anggota"
+        Column2.Name = "Column2"
+        ' 
+        ' Column3
+        ' 
+        Column3.HeaderText = "Asal Kota"
+        Column3.Name = "Column3"
+        ' 
+        ' Column4
+        ' 
+        Column4.HeaderText = "Telp"
+        Column4.Name = "Column4"
+        ' 
+        ' txtnim
+        ' 
+        txtnim.Location = New Point(171, 30)
+        txtnim.Name = "txtnim"
+        txtnim.Size = New Size(414, 23)
+        txtnim.TabIndex = 3
+        ' 
+        ' txtnama
+        ' 
+        txtnama.Location = New Point(171, 59)
+        txtnama.Name = "txtnama"
+        txtnama.Size = New Size(414, 23)
+        txtnama.TabIndex = 3
+        ' 
+        ' txtalamat
+        ' 
+        txtalamat.Location = New Point(171, 93)
+        txtalamat.Name = "txtalamat"
+        txtalamat.Size = New Size(414, 23)
+        txtalamat.TabIndex = 3
+        ' 
+        ' txttelp
+        ' 
+        txttelp.Location = New Point(171, 126)
+        txttelp.Name = "txttelp"
+        txttelp.Size = New Size(414, 23)
+        txttelp.TabIndex = 3
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(68, 170)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 7
+        Button1.Text = "simpan"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(186, 170)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 8
+        Button2.Text = "hapus"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(310, 170)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(75, 23)
+        Button3.TabIndex = 9
+        Button3.Text = "edit"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(txttelp)
+        Controls.Add(txtalamat)
+        Controls.Add(txtnama)
+        Controls.Add(txtnim)
+        Controls.Add(DataGridView1)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(btmkoneksi)
+        Name = "Form1"
+        Text = "Form1"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btmkoneksi As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents txtnim As TextBox
+    Friend WithEvents txtnama As TextBox
+    Friend WithEvents txtalamat As TextBox
+    Friend WithEvents txttelp As TextBox
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As DataGridViewTextBoxColumn
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,129 @@
+﻿Imports System.Data.Odbc
+Public Class Form1
+    Dim conn As OdbcConnection
+    Dim dr As OdbcDataReader
+    Dim da As OdbcDataAdapter
+    Dim cmd As OdbcCommand
+
+    Sub koneksi()
+        conn = New OdbcConnection("dsn=dsn_db_aplikasi")
+        Try
+            If conn.State = ConnectionState.Closed Then
+                conn.Open()
+                MsgBox("Koneksi Berhasil", MsgBoxStyle.Exclamation, "informasi")
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show("Koneksi Gagal" & ex.Message)
+        End Try
+    End Sub
+    Sub bersihkan()
+        txtnama.Clear()
+        txtalamat.Clear()
+        txttelp.Clear()
+        txtnim.Clear()
+    End Sub
+
+    Sub tambahdata()
+        Try
+            Call koneksi()
+            Dim input_nama As String = txtnama.Text
+            Dim input_alamat As String = txtalamat.Text
+            Dim input_telp As String = txttelp.Text
+
+            Dim query As String = "INSERT INTO tbl_anggota (namaanggota, alamatanggota, telpanggota) VALUES (?,?,?)"
+
+            cmd = New OdbcCommand(query, conn)
+            cmd.Parameters.AddWithValue("namaanggota", input_nama)
+            cmd.Parameters.AddWithValue("alamatanggota", input_alamat)
+            cmd.Parameters.AddWithValue("telpanggota", input_telp)
+            cmd.ExecuteNonQuery()
+            MsgBox("Data berhasil disimpan!!")
+            tampildata()
+            bersihkan()
+
+        Catch ex As Exception
+            MsgBox("Terjadi kesalahan: " & ex.Message)
+        Finally
+            conn.Close()
+        End Try
+    End Sub
+
+    Sub tampildata()
+        Call koneksi()
+        cmd = New OdbcCommand("select * from tbl_anggota", conn)
+        dr = cmd.ExecuteReader
+        DataGridView1.Rows.Clear()
+        Do While dr.Read = True
+            DataGridView1.Rows.Add(dr(0), dr(1), dr(2), dr(3))
+        Loop
+    End Sub
+    Sub hapusdata()
+        Call koneksi()
+        Dim kodeanggota As String = txtnim.Text
+        Dim sql As String = "DELETE FROM tbl_anggota WHERE kodeanggota = ?"
+        Try
+            Dim cmd As New OdbcCommand(sql, conn)
+            cmd.Parameters.AddWithValue("kodeanggota", kodeanggota)
+            cmd.ExecuteNonQuery()
+            MsgBox("Data berhasil dihapus")
+        Catch ex As Exception
+            MsgBox("Gagal: " & ex.Message)
+        End Try
+    End Sub
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btmkoneksi.Click
+        Call koneksi()
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+        txtnim.Text = DataGridView1.Rows(e.RowIndex).Cells(0).Value
+        txtnama.Text = DataGridView1.Rows(e.RowIndex).Cells(1).Value
+        txtalamat.Text = DataGridView1.Rows(e.RowIndex).Cells(2).Value
+        txttelp.Text = DataGridView1.Rows(e.RowIndex).Cells(3).Value
+    End Sub
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        tampildata()
+    End Sub
+
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        Call tambahdata()
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Call hapusdata()
+        Call bersihkan()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim kodeanggota As String = txtnim.Text
+        Dim namaanggota As String = txtnama.Text
+        Dim alamatanggota As String = txtalamat.Text
+        Dim nomortelp As String = txttelp.Text
+        Dim sql As String = "UPDATE tbl_anggota SET namaanggota = ?, alamatanggota = ?, telpanggota = ? WHERE kodeanggota = ?"
+        Try
+            Call koneksi()
+            Dim cmd As New OdbcCommand(sql, conn)
+            cmd.Parameters.AddWithValue("namaanggota", namaanggota)
+            cmd.Parameters.AddWithValue("alamatanggota", alamatanggota)
+            cmd.Parameters.AddWithValue("telpanggota", nomortelp)
+            cmd.Parameters.AddWithValue("kodeanggota", kodeanggota)
+            cmd.ExecuteNonQuery()
+            MsgBox("Data berhasil diubah")
+            tampildata()
+            bersihkan()
+        Catch ex As Exception
+            MsgBox("Gagal: " & ex.Message)
+        Finally
+            conn.Close()
+        End Try
+
+        MsgBox("Pilih satu baris data yang akan diubah")
+
+    End Sub
+End Class
